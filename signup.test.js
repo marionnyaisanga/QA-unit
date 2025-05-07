52aa00733c98952bfac54e2a46bc98b4e783d6ba
Marion <marionnyaisanga@gmail.com>
2025-05-07 06:04:33 +0300

    commit

--- signup.test.js
+++ signup.test.js
@@ -16,10 +16,27 @@ describe("validateEmail",()=>{
 
 describe("submitForm",()=>{
     it("should submit from with valid data",async()=>{
-        global.fetch = jsxEmptyExpression.fn().mocklmplementatiooon(()=>
+        global.fetch = jest.fn().mocklmplementation(()=>
         Promise.resolve({
             ok:true,
-            json:()=> Promise.resolve({message: "Success"},)
-        }))
-    })
-})
+            json:()=> Promise.resolve({message: "Success"}),
+        })
+    );
+    const response = await submitForm("text@example.com","password");
+    expect(response.message).toBe("Succes");
+    expect(fetch).toHaveBeenCalledTimes(1);
+    });
+    it("should throw errror for invalid email",async()=>{
+        await expect(submitForm("invalid email","password")).regects.toThrow("Invalid email format");
+
+    });
+    it("should handle API error",async()=>{
+        global.fetch = jest.fn().mocklmplementation(()=>
+        Promise.resolve({
+            ok:false,
+            statusText: "Server error",
+        })
+    );
+    await expect(submitForm ("test@example.com","password")).rejects.toThrow("signup failed");
+    });
+});
